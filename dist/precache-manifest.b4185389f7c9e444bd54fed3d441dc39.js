ab05e3c121dccb429fd8526315ec8ffab383d57d
Dmitry <dikck.dmitry@yandex.ru>
2021-10-29 13:59:48 +0400

    add the project

---
+++ dist/precache-manifest.b4185389f7c9e444bd54fed3d441dc39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd0befbfec8608d0586e0857db718fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9873fb7b57b7c17f0b30",
+    "url": "/js/app.b5a6d2a1.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
